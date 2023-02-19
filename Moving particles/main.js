1bc17fbc18565e11d1002c2a1d5ccf8d52928fc1
Wisse <wisse.raaijmakers@gmail.com>
2023-02-19 16:57:44 +0100

    size and color

--- Moving particles/main.js
+++ Moving particles/main.js
@@ -7,12 +7,18 @@ let canvas = document.getElementById("canvas");
 // Make n particles
 
 let particle_array = [];
-let color_array = ["red","green","blue","yellow","magenta"];
+let color_array = ["#db222a","#098d9b","#053c5e","#bc1055","#fa600c"];
 
 for (let index = 0; index < 100; index++) {
-    let x = Math.floor(Math.random() * 100) + 1;
-    let y = Math.floor(Math.random() * 100) + 1;
-    particle_array.push (new Particle(x, y, color_array[index], 1))
+    
+    //generating random values for particle properties
+    let x = Math.floor(Math.random() * canvas.width) + 1;
+    let y = Math.floor(Math.random() * canvas.height) + 1;
+    let size = Math.floor(Math.random() * 30) + 1;
+    let random_color = Math.floor(Math.random() * color_array.length);
+
+    //generating new particle in particle array
+    particle_array.push (new Particle(x, y, color_array[random_color], size))
     
 }
 
