5bd4482cb49655eb803ef2e7ca76aa8006e31b3a
Wisse <wisse.raaijmakers@gmail.com>
2023-02-19 16:41:06 +0100

    particle array

--- Moving particles/main.js
+++ Moving particles/main.js
@@ -9,7 +9,7 @@ let canvas = document.getElementById("canvas");
 let particle_array = [];
 let color_array = ["red","green","blue","yellow","magenta"];
 
-for (let index = 0; index < 5; index++) {
+for (let index = 0; index < 100; index++) {
     let x = Math.floor(Math.random() * 100) + 1;
     let y = Math.floor(Math.random() * 100) + 1;
     particle_array.push (new Particle(x, y, color_array[index], 1))
@@ -22,7 +22,7 @@ console.log(particle_array)
 // Draw particles
 let renderer = new Renderer(canvas);
 
-// renderer.draw_particle_array(array);
+renderer.draw_particle_array(particle_array);
 
 
 
