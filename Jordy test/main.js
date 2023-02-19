4bb8c86ab0aa9bdcfbd39ee1bfd96c825dc48c60
Jordy de Lange <mrshroom1988@gmail.com>
2023-02-19 09:33:48 +0100

    tested a bit

---
+++ Jordy test/main.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+
+const square = document.getElementById("square");
+console.log(square.getAttributeNames());
+square.style.borderWidth = "6px"
+
+
+// Initialize the rotation angle to 0
+let angle = 0;
+
+// Define a function to update the square's rotation
+function rotateSquare() {
+  // Increment the angle by 5 degrees
+  angle += 5;
+
+  // Apply the rotation transform to the square
+  square.style.transform = `rotate(${angle}deg)`;
+}
+
+// Call rotateSquare every 50 milliseconds to animate the rotation
+setInterval(rotateSquare, 50);
