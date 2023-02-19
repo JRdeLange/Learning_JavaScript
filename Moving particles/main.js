9cba29efc93a5e36ac7301542df43651d944aa57
Wisse <wisse.raaijmakers@gmail.com>
2023-02-19 16:37:28 +0100

    de grote particledans

--- Moving particles/main.js
+++ Moving particles/main.js
@@ -5,6 +5,18 @@ import Particle from "http://localhost:5500/Moving particles/particle.js"
 
 // Make n particles
 
+let particle_array = [];
+let color_array = ["red","green","blue","yellow","magenta"];
+
+for (let index = 0; index < 5; index++) {
+    let x = Math.floor(Math.random() * 100) + 1;
+    let y = Math.floor(Math.random() * 100) + 1;
+    particle_array.push (new Particle(x, y, color_array[index], 1))
+    
+}
+
+console.log(particle_array)
+
 
 // Draw particles
 
