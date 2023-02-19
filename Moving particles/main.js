37a90f8b0f7712c7c98ce40e8d774f49908cfc95
Jordy de Lange <mrshroom1988@gmail.com>
2023-02-19 16:35:50 +0100

    added prototype particle renderer

--- Moving particles/main.js
+++ Moving particles/main.js
@@ -1,10 +1,16 @@
 import Particle from "http://localhost:5500/Moving particles/particle.js"
+import Renderer from "http://localhost:5500/Moving particles/renderer.js"
 
 // get HTML5 canvas
-
+let canvas = document.getElementById("canvas");
 
 // Make n particles
 
 
 // Draw particles
+let renderer = new Renderer(canvas);
+
+renderer.draw_particle_array(test_array);
+
+
 
