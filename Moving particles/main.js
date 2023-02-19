6b19a310d2d26a45569f211ede56662cc8d2d216
Jordy de Lange <mrshroom1988@gmail.com>
2023-02-19 15:57:17 +0100

    particle skeleton

--- Moving particles/main.js
+++ Moving particles/main.js
@@ -1,26 +1,10 @@
 import Particle from "http://localhost:5500/Moving particles/particle.js"
 
-
-const square = document.getElementById("square");
-square.style.borderWidth = "6px"
+// get HTML5 canvas
 
 
+// Make n particles
 
 
+// Draw particles
 
-
-// Initialize the rotation angle to 0
-let angle = 0;
-
-// Define a function to update the square's rotation
-function rotateSquare() {
-  // Increment the angle by 5 degrees
-  angle += 1;
-
-  // Apply the rotation transform to the square
-  // mover.move(square)
-  square.style.transform = "rotate(" + angle + "deg)"
-}
-
-// Call rotateSquare every 50 milliseconds to animate the rotation
-setInterval(rotateSquare, 1000/60);
--- Moving particles/particle.js
+++ Moving particles/particle.js
@@ -1,18 +1,9 @@
 export default class Particle {
 
     constructor(x, y, color, size) {
-        this.x_position = x;
+        this.x = x;
         this.y = y;
         this.color = color;
         this.size = size;
     }
-
-    zeg() {
-        console.log(this.id); //yo ik zet hier iets neer
-    }
-} // comment
-
-
-
-
-// HOWOEFPISDFAISDOFHASJF
+}
