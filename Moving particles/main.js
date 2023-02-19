24964bedb2225692330517cfcd11b842e05d5cff
Wisse <wisse.raaijmakers@gmail.com>
2023-02-19 18:48:38 +0100

    merging

--- Moving particles/main.js
+++ Moving particles/main.js
@@ -14,40 +14,47 @@ let image = new Pixels("http://localhost:5500/Moving particles/test.png", image_
 
 // Image is now loaded
 function image_is_loaded(){
-
-  // Make n particles
-  let particle_array = [];
-  let color_array = ["#db222a","#098d9b","#053c5e","#bc1055","#fa600c"];
-
-  for (let index = 0; index < 100; index++) {
-    // generate position based on image
-
-      
-    //generating random values for particle properties
-    let x = Math.floor(Math.random() * canvas.width) + 1;
-    let y = Math.floor(Math.random() * canvas.height) + 1;
-    let size = Math.floor(Math.random() * 100) + 1;
-    let random_color = Math.floor(Math.random() * color_array.length);
-
-    //generating new particle in particle array
-    particle_array.push (new Particle(x, y, color_array[random_color], size));
-      
+    console.log(image.pixels[0][0])
+  
+  
+  
+    // Make n particles
+    let particle_array = [];
+    let color_array = ["#db222a","#098d9b","#053c5e","#bc1055","#fa600c"];
+  
+    for (let index = 0; index < 500; index++) {
+      // generate position based on image
+  
+        
+      //generating random values for particle properties
+      let x = Math.floor(Math.random() * canvas.width) + 1;
+      let y = Math.floor(Math.random() * canvas.height) + 1;
+      let size = Math.floor(Math.random() * 100) + 1;
+      let random_color = Math.floor(Math.random() * color_array.length);
+  
+      //generating new particle in particle array
+      particle_array.push (new Particle(x, y, color_array[random_color], size));
+        
+    }
+  
+  
+    
+  
+    // Draw particles
+    let renderer = new Renderer(canvas, particle_array);
+    let mover = new Mover(particle_array);
+  
+    setInterval(draw, 1/60);
+  
+  
+    function draw() {
+      renderer.clear();
+      particle_array.forEach(particle => {
+        //particle.move()
+        particle.dodge(mouse)
+      });
+      renderer.draw_particle_array();
+  
+  
+    }
   }
-
-  // Draw particles
-  let renderer = new Renderer(canvas, particle_array);
-  let mover = new Mover(particle_array);
-
-  setInterval(draw, 1/60);
-
-
-  function draw() {
-    renderer.clear();
-    particle_array.forEach(particle => {
-      particle.move()
-    });
-    renderer.draw_particle_array();
-
-
-  }
-}
